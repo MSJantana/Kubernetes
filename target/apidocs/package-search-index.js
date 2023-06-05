7e14c4ef63102c1a00dce5a9f84991e3691e3344
marcio <marcio@10.12.1.72>
2023-06-05 13:08:22 -0300

    Atualização dos Arquivos

--- target/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.msoft.hubeducacional"},{"l":"com.msoft.hubeducacional.controller"},{"l":"com.msoft.hubeducacional.model"}];updateSearchResults();
