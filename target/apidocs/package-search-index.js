63edf603b1ed6e7510c322397e1872ad71d213d9
marcio <marcio@10.12.1.72>
2024-02-21 09:53:06 -0300

    Atualização_2024

---
+++ target/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.msoft.hubeducacional"},{"l":"com.msoft.hubeducacional.controller"},{"l":"com.msoft.hubeducacional.model"}];updateSearchResults();
